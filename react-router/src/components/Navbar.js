bdb3d0b867717870574726bec8174a12b3c6f6f1
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-30 18:16:40 -0300

    Aula102

--- react-router/src/components/Navbar.js
+++ react-router/src/components/Navbar.js
@@ -1,14 +1,19 @@
 // 2 - links com react router
 import './Navbar.css';
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>Sobre</Link>
-    </nav>
+        {/*<Link to='/'>Home</Link>
+        <Link to='/about'>Sobre</Link>*/}
+        <NavLink to='/' //</nav>className={({isActive}) => (isActive ? 'está ativo' : 'não está ativo')} 
+        >
+          Home
+          </NavLink>
+        <NavLink to='/about'>Sobre</NavLink>
+  </nav>
   )
 }
 
