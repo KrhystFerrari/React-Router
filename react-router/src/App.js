0cecc4730141662f0b96e866b8acff7175ea7566
Krhyst Ferrari <krhys_ferrari@hotmail.com>
2022-10-30 19:05:45 -0300

    Aula104

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 // 1 - config react router
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
@@ -33,6 +33,8 @@ function App() {
           <Route path='/products/:id/info' element={<Info />} />
           {/* 9 - search */}
           <Route path='/search' element={<Search />} />
+          {/* 10 - Redirect */}
+          <Route path='/company' element={<Navigate to='/about' />} />
           {/* 7 - no match route */}
           <Route path='*' element={<NotFound />} />
         </Routes>
